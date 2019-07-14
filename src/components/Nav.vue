f17b401849b95be69b884df9338c30d8df1e68bf
Wouter Kayser <wouterkayser@gmail.com>
2019-07-14 14:32:43 +0200

    changing out the picture for jpg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,7 +5,7 @@
       <span class="d-none d-lg-block">
         <g-image
           alt="Image of Wouter"
-          src="../assets/images/profile.png"
+          src="../assets/images/profile.jpg"
           class="img-fluid img-profile rounded-circle mx-auto mb-2"
         />
       </span>
